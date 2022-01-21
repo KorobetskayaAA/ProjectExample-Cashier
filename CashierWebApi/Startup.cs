bb16eb7ed52000a79dd9503a9444afdc5421e79e
A. Korobetskaya <kornast@yandex.ru>
2022-01-21 22:55:49 +0400

    Настроен файловый сервер для фронта

--- CashierWebApi/Startup.cs
+++ CashierWebApi/Startup.cs
@@ -66,10 +66,13 @@ namespace CashierWebApi
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cashier API V1");
-                c.RoutePrefix = string.Empty;
+                c.RoutePrefix = "swagger";
             });
 
             app.UseHttpsRedirection();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
